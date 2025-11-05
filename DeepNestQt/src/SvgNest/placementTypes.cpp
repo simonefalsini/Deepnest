c2c79d24d89a1f4e4d7cf9ab2a1c49d28ae9430b
simone falsini <simone@devstudio.it>
2025-11-05 12:06:34 +0100

    removed conversion tes made from jules

--- DeepNestQt/src/SvgNest/placementTypes.cpp
+++
@@ -1,2 +0,0 @@
-#include "placementTypes.h"
-// No specific code needed yet as header is placeholder only
--- DeepNestQt/src/SvgNest/placementTypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLACEMENTTYPES_H
-#define PLACEMENTTYPES_H
-
-// Placeholder for enums, structs related to placement results, etc.
-// For example, if SvgNest::PlacedPart or SvgNest::NestSolution were to become more complex
-// and used by many internal classes, parts of them could be defined here.
-// For now, it can be minimal as SvgNest.h already defines PlacedPart and NestSolution.
-
-#endif // PLACEMENTTYPES_H
